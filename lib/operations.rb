517b8c3d42712087a37a2f6f8949ec0c5ddcf78b
Thanh Pham <tpham0123@gmail.com>
2020-05-09 20:15:47 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,12 @@
 def unsafe?(speed)
+  speed < 40 || speed > 60  
 
 end
+unsafe?(speed=30)
 
 
-
-def not_safe?(speed)
+def not_safe?(speed = 30)
+  speed < 40 || speed > 60 ? true : false
 	
 end
 	
